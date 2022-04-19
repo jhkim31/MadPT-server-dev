0e068b155dea992d83e6eca4e53e2b2859f7c80f
jhkim31 <biz.jh.kim@gmail.com>
2022-04-19 15:14:17 +0900

    CICD: #18 Docker 스크립트 수정 AWS 배포시 실행되는 Docker Script 수정
    현재 진행중인 컨테이너 멈추는 Script/container_stop.sh 삭제
    이미지 당겨오는 Script/image_pull.sh 삭제
    컨테이너 실행시키는 Script/new_container_run.sh 삭제
    
    모든 기능을 통합한 Script/docker_script.sh 및
    특정 컨테이너만 삭제하는 docker_stop_script.py 스크립트 추가

---
+++ Script/docker_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ls
+docker pull jhkimdocker/capstone_api_image:MADPT_VERSION
+docker pull jhkimdocker/capstone_db_image:MADPT_VERSION
+
+python3 docker_script/api_container_stop_script.py
+# docker stop $(docker ps -a -q)
+
+docker run -d -p 8080:8080 --name api_MADPT_VERSION jhkimdocker/capstone_api_image:MADPT_VERSION
+# docker run -d -p 3306:3306 --name db_MADPT_VERSION jhkimdocker/capstone_db_image:MADPT_VERSION
