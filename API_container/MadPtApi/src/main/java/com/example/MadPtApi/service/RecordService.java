ed4e0d3fa522ad874df22e9941ec07bb5a252151
Jung Rock <jlal1226@naver.com>
2022-04-27 20:28:53 +0900

    FEAT : implement Record Core

--- API_container/MadPtApi/src/main/java/com/example/MadPtApi/service/RecordService.java
+++ API_container/MadPtApi/src/main/java/com/example/MadPtApi/service/RecordService.java
@@ -1,7 +1,11 @@
 package com.example.MadPtApi.service;
 
+import com.example.MadPtApi.domain.Exercise;
+import com.example.MadPtApi.domain.ExerciseData;
 import com.example.MadPtApi.domain.Member;
 import com.example.MadPtApi.domain.Record;
+import com.example.MadPtApi.dto.RecordRequestDto;
+import com.example.MadPtApi.repository.ExerciseRepository;
 import com.example.MadPtApi.repository.RecordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,15 +22,38 @@ public class RecordService {
 
     private final RecordRepository recordRepository;
     private final MemberService memberService;
+    private final ExerciseRepository exerciseRepository;
 
     /**
      * 운동 정보 저장
      */
     @Transactional
-    public Long saveRecord(Record record) {
-        recordRepository.save(record);
-        return record.getId();
+    public void saveRecord(Long memberId, List<RecordRequestDto> recordRequestDtoList) {
+        // 회원 엔티티
+        Member member = memberService.findMember(memberId);
+
+        for (RecordRequestDto dto : recordRequestDtoList) {
+            Exercise exercise = null;
+            Optional<Exercise> exerciseResult = exerciseRepository.findById(dto.getExerciseId());
+            if (exerciseResult.isPresent()) {
+                exercise = exerciseResult.get();
+            }
+            ExerciseData exerciseData = ExerciseData.builder()
+                    .reps(dto.getReps())
+                    .build();
+
+            Record record = Record.builder()
+                    .member(member)
+                    .exercise(exercise)
+                    .startTime(dto.getStartTime())
+                    .endTime(dto.getEndTime())
+                    .exerciseData(exerciseData)
+                    .build();
+
+            recordRepository.save(record);
+        }
     }
+
 
     /**
      * 날짜별 운동 정보 조회
