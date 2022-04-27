e3d74f4ffc42ed06156e5fed30bca2b7332877bb
Jung Rock <jlal1226@naver.com>
2022-04-27 20:24:40 +0900

    FEAT : Exercise Record core

---
+++ API_container/MadPtApi/src/test/java/com/example/MadPtApi/service/RecordServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.MadPtApi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecordServiceTest {
+    @Test
+    public void 운동_정보_저장() throws Exception {
+        // given
+
+        // when
+        // then
+    }
+
+}
