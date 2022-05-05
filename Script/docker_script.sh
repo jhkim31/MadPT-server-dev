b0edc8985ceb955f4cc437134b891a828073d1c0
jhkim31 <biz.jh.kim@gmail.com>
2022-05-06 02:34:41 +0900

    CICD: API 컨테이너 자동실행 중지
    bash 쉘만 실행하여, 내부 네트워크 수정을 위해 자동 실행 중지함.

--- Script/docker_script.sh
+++ Script/docker_script.sh
@@ -7,5 +7,5 @@ docker stop $(docker ps -aq -f=name=api_)
 docker stop $(docker ps -aq -f=name=db_)
 
 docker run -d -p 3306:3306 --name db_MADPT_VERSION jhkimdocker/capstone_db_image:MADPT_VERSION
-docker run -d -p 8080:8080 --name api_MADPT_VERSION --link db_MADPT_VERSION jhkimdocker/capstone_api_image:MADPT_VERSION
+docker run -d -p 8080:8080 --name api_MADPT_VERSION -it jhkimdocker/capstone_api_image:MADPT_VERSION bash
 
