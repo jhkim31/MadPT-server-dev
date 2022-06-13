e2fd5f681f2239ca388c22af4c6183e979657b1f
jhkim31 <biz.jh.kim@gmail.com>
2022-06-13 17:44:27 +0900

    CICD: TEST

--- Script/update_script.sh
+++ Script/update_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker pull jhkimdocker/test:MADPT_VERSION
+kubectl set image deployment/get-api-deployment get-api=jhkimdocker/capstone_get_api_image:MADPT_VERSION
+kubectl set image deployment/post-api-deployment post-api=jhkimdocker/capstone_post_api_image:MADPT_VERSION
