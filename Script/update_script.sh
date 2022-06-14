58cb26deec1d2df6f54f592e805940c001ea3833
jhkim31 <biz.jh.kim@gmail.com>
2022-06-14 13:07:34 +0900

    CICD : capstone

--- Script/update_script.sh
+++ Script/update_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl set image deployment/get-api-deployment get-api=jhkimdocker/capstone_get_api_image:MADPT_VERSION
+kubectl set image deployment/post-api-deployment get-api=jhkimdocker/capstone_get_api_image:MADPT_VERSION
 kubectl set image deployment/post-api-deployment post-api=jhkimdocker/capstone_post_api_image:MADPT_VERSION
